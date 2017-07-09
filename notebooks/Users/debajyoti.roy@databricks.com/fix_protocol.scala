9ce334fc2e9cbd64b1d9cad1209c5c45a2d5bdbd
Debajyoti Roy <roy@databricks.com>
2017-07-09 04:53:50 -0400

    final

--- notebooks/Users/debajyoti.roy@databricks.com/fix_protocol.scala
+++ notebooks/Users/debajyoti.roy@databricks.com/fix_protocol.scala
@@ -17,6 +17,10 @@ dbutils.fs.rm("/mnt/roy/sample_fix")
 
 // COMMAND ----------
 
+// MAGIC %md #Ingested Data
+
+// COMMAND ----------
+
 dbutils.fs.put("/mnt/roy/sample_fix", """
 8=FIX.4.19=11235=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=157
 8=FIX.4.19=15435=649=BRKR56=INVMGR34=23652=19980604-07:58:4823=11568528=N55=SPMI.MI54=227=20000044=10100.00000025=H10=159
@@ -145,6 +149,10 @@ fixJsonDS.selectExpr("toTime(sending_time) gmt_time", "cast (price as int) price
 
 // COMMAND ----------
 
+// MAGIC %md #Curated Data
+
+// COMMAND ----------
+
 // MAGIC %sql select * from fix_table
 
 // COMMAND ----------
@@ -154,11 +162,11 @@ fixJsonDS.selectExpr("toTime(sending_time) gmt_time", "cast (price as int) price
 
 // COMMAND ----------
 
-// MAGIC %sql select count(1), sender_company from fix_table group by sender_company having sender_company <> ''
+// MAGIC %sql select count(1), sender_company from fix_table group by sender_company
 
 // COMMAND ----------
 
-// MAGIC %sql select sum(price), ticker, sender_company from fix_table group by ticker, sender_company having ticker <> ''
+// MAGIC %sql select sum(price), ticker, sender_company from fix_table group by ticker, sender_company
 
 // COMMAND ----------
 
